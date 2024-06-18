d8c4ef588365913238fbc0c372169f5321cbae1d
BrownBear85 <91921122+BrownBear85@users.noreply.github.com>
2024-06-18 14:25:29 -0700

    simplified lang keys

--- src/main/java/com/bonker/swordinthestone/datagen/SSLanguageProvider.java
+++ src/main/java/com/bonker/swordinthestone/datagen/SSLanguageProvider.java
@@ -21,18 +21,18 @@ public class SSLanguageProvider extends LanguageProvider {
         addEntityType(SSEntityTypes.ENDER_RIFT, "Ender Rift");
         addEntityType(SSEntityTypes.SPELL_FIREBALL, "Fireball");
 
-        add(SwordAbilities.THUNDER_SMITE.get(), "Thunder Smite", "Thundering %1$s", "Sword charges when attacking mobs\nStrikes mobs with lightning when fully charged");
-        add(SwordAbilities.VAMPIRIC.get(), "Life Steal", "Vampiric %1$s", "Steal some of an enemy's health upon killing it\n(Capped at 5 hearts)");
-        add(SwordAbilities.TOXIC_DASH.get(), "Toxic Dash", "Toxic %1$s", "Slash forward on right click leaving behind a poisonous cloud");
-        add(SwordAbilities.ENDER_RIFT.get(), "Ender Rift", "Unstable %1$s", "Hold right click and move your mouse to move an orb\nRelease to teleport to the orb");
-        add(SwordAbilities.FIREBALL.get(), "Fireball", "Flaming %1$s", "Hold right click to charge up a fireball attack");
-        add(SwordAbilities.DOUBLE_JUMP.get(), "Double Jump", "Aetherial %1$s", "While holding, tap space in the air to double jump\nFall damage is reduced while holding");
-        add(SwordAbilities.ALCHEMIST.get(), "Alchemist", "Chemical %1$s", "Inflicts a negative status effect on hit occasionally\nGrants the user a positive status effect on kill occasionally");
-        add(SwordAbilities.BAT_SWARM.get(), "Bat Swarm", "Unholy %1$s", "Right click to ride a swarm of bats that attacks mobs");
+        add(SwordAbilities.THUNDER_SMITE.get(), "Thunder Smite", "Thundering %s", "Sword charges when attacking mobs\nStrikes mobs with lightning when fully charged");
+        add(SwordAbilities.VAMPIRIC.get(), "Life Steal", "Vampiric %s", "Steal some of an enemy's health upon killing it\n(Capped at 5 hearts)");
+        add(SwordAbilities.TOXIC_DASH.get(), "Toxic Dash", "Toxic %s", "Slash forward on right click leaving behind a poisonous cloud");
+        add(SwordAbilities.ENDER_RIFT.get(), "Ender Rift", "Unstable %s", "Hold right click and move your mouse to move an orb\nRelease to teleport to the orb");
+        add(SwordAbilities.FIREBALL.get(), "Fireball", "Flaming %s", "Hold right click to charge up a fireball attack");
+        add(SwordAbilities.DOUBLE_JUMP.get(), "Double Jump", "Aetherial %s", "While holding, tap space in the air to double jump\nFall damage is reduced while holding");
+        add(SwordAbilities.ALCHEMIST.get(), "Alchemist", "Chemical %s", "Inflicts a negative status effect on hit occasionally\nGrants the user a positive status effect on kill occasionally");
+        add(SwordAbilities.BAT_SWARM.get(), "Bat Swarm", "Unholy %s", "Right click to ride a swarm of bats that attacks mobs");
 
-        add("ability.swordinthestone.alchemist.victim", "Alchemist: Inflicted victim with %1$s");
-        add("ability.swordinthestone.alchemist.self", "Alchemist: Applied %1$s to self");
-        add("ability.swordinthestone.bat_swarm.name", "%1$s's %2$s");
+        add("ability.swordinthestone.alchemist.victim", "Alchemist: Inflicted victim with %s");
+        add("ability.swordinthestone.alchemist.self", "Alchemist: Applied %s to self");
+        add("ability.swordinthestone.bat_swarm.name", "%s's %s");
     }
 
     private void add(SwordAbility key, String name, String title, String description) {
